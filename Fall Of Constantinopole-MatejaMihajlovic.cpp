06183876d2aedc91665f61a57f0b6e97b599a2c5
Najjacism Gaff <najjacismgaff@gmail.com>
2022-06-18 15:49:25 +0200

    Dodat  Uvod Male Osmanlije i "Carigradjani"??

--- Fall Of Constantinopole-MatejaMihajlovic.cpp
+++ Fall Of Constantinopole-MatejaMihajlovic.cpp
@@ -14,14 +14,34 @@ int main()
     ALLEGRO_EVENT_QUEUE* queue;
     ALLEGRO_TIMER* timer;
     ALLEGRO_BITMAP* carigrad = NULL;
+    ALLEGRO_BITMAP* uvod = NULL;
+
     ALLEGRO_SAMPLE* muzika = NULL;
     ALLEGRO_SAMPLE_INSTANCE* muzika_sample = NULL;
+
+    ALLEGRO_BITMAP* carigrad1 = NULL;
+    ALLEGRO_BITMAP* ottoman1 = NULL;
+    ALLEGRO_BITMAP* carigrad2 = NULL;
+    ALLEGRO_BITMAP* ottoman2 = NULL;
+    ALLEGRO_BITMAP* carigrad3 = NULL;
+    ALLEGRO_BITMAP* ottoman3 = NULL;
+    ALLEGRO_BITMAP* ottoman4 = NULL;
+
+    ALLEGRO_BITMAP* carigrad4 = NULL;
+    ALLEGRO_BITMAP* ottoman5 = NULL;
+
 
 
     al_init();
     display = al_create_display(1000, 700);
     queue = al_create_event_queue();
     timer = al_create_timer(1.0 / 60);
+
+
+    al_install_mouse();
+    al_init_image_addon();
+    al_init_font_addon();
+    al_init_ttf_addon();
 
 
     al_install_audio();
@@ -38,11 +58,38 @@ int main()
     al_init_image_addon();
 
 
+    uvod = al_load_bitmap("Uvod.jpg");
+    assert(uvod != NULL);
+
+
     carigrad = al_load_bitmap("KaoCarigrad.jpg");
     assert(carigrad != NULL);
 
-    int width = al_get_display_width(display);
 
+
+    carigrad1 = al_load_bitmap("CARIGRAD1.png");
+    assert(carigrad1 != NULL);
+    carigrad2 = al_load_bitmap("CARIGRAD1.png");
+    assert(carigrad2 != NULL);
+    carigrad3 = al_load_bitmap("CARIGRAD1.png");
+    assert(carigrad3 != NULL);
+    carigrad4 = al_load_bitmap("CARIGRAD1.png");
+    assert(carigrad4 != NULL);
+
+    ottoman1 = al_load_bitmap("OTTOMAN1.png");
+    assert(ottoman1 != NULL);
+    ottoman2 = al_load_bitmap("OTTOMAN1.png");
+    assert(ottoman2 != NULL);
+    ottoman3 = al_load_bitmap("OTTOMAN1.png");
+    assert(ottoman3 != NULL);
+    ottoman4 = al_load_bitmap("OTTOMAN1.png");
+    assert(ottoman4 != NULL);
+    ottoman5 = al_load_bitmap("OTTOMAN1.png");
+    assert(ottoman5 != NULL);
+
+
+
+    int width = al_get_display_width(display);
 
     al_start_timer(timer);
 
@@ -53,18 +100,56 @@ int main()
      muzika = al_load_sample("CeddinDeden.ogg");
      al_play_sample(muzika, 1.0, 0, 1, ALLEGRO_PLAYMODE_LOOP, NULL);
 
+     while (running) {
+         al_flip_display();
+
+         al_draw_bitmap(uvod, 0, 0, 0);
+
+         ALLEGRO_EVENT event;
+         al_wait_for_event(queue, &event);
+
+     
+         if (event.type == ALLEGRO_EVENT_KEY_UP || event.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
+             goto kraj_uvoda;
+        // else
+             //goto prt;
+
+     }
+
+     kraj_uvoda:
+
     while (running) {
         al_flip_display();
 
         ALLEGRO_EVENT event;
         al_wait_for_event(queue, &event);
-        if (event.type == ALLEGRO_EVENT_KEY_UP || event.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
-            running = false;
+        //if (event.type == ALLEGRO_EVENT_KEY_UP || event.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
+            //running = false;
 
         if (event.type == ALLEGRO_EVENT_TIMER){
                 
                 al_clear_to_color(al_map_rgba_f(1, 1, 1, 1));
                 al_draw_bitmap(carigrad,0, 0,0);
+
+                printf("aaaaa/t");
+
+                al_draw_bitmap(ottoman1, 6, 300, 0);
+                al_draw_bitmap(carigrad1, 120, 300, 0);
+
+                al_draw_bitmap(ottoman2, 160, 40, 0);
+                al_draw_bitmap(carigrad2, 270, 120, 0);
+
+                al_draw_bitmap(ottoman3, -15, 500, 0);
+                al_draw_bitmap(carigrad3, 90, 500, 0);
+
+                al_draw_bitmap(ottoman4, 90, 160, 0);
+                
+
+
+                al_draw_bitmap(ottoman5, 600, 100, 0);
+                al_draw_bitmap(carigrad4, 600, 220, 0);
+
+
                 al_flip_display();
         }
 
