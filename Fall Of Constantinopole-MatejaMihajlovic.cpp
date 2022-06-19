a9bd92ce3f082b76b34996dd876dbb2a57972067
Najjacism Gaff <najjacismgaff@gmail.com>
2022-06-19 16:07:40 +0200

    Poprilicno gotovo? Dodati vizantijski napadi

--- Fall Of Constantinopole-MatejaMihajlovic.cpp
+++ Fall Of Constantinopole-MatejaMihajlovic.cpp
@@ -22,7 +22,12 @@ int main()
     ALLEGRO_BITMAP* CRX = NULL;
     ALLEGRO_BITMAP* uvod = NULL;
     ALLEGRO_BITMAP* turki = NULL;
+    ALLEGRO_BITMAP* boom = NULL;
 
+    //napadi/prestupi
+
+    ALLEGRO_BITMAP* otatt = NULL;
+    ALLEGRO_BITMAP* byatt = NULL;
 
     //zvuci
     ALLEGRO_SAMPLE* muzika = NULL;
@@ -49,6 +54,11 @@ int main()
 
     int c1 = 17, c2 = 32, c3 = 24, c4 = 20;
     int x1 = 0, x2 = 0, x3 = 0, x4 = 0;
+
+    int o1 = 23, o2 = 32, o3 = 30, o4 = 20, o5 = 17;
+    int ox1 = 0, ox2 = 0, ox3 = 0, ox4 = 0, ox5 = 0;
+
+    int sek = 0;
 
     int prc = 0;//prc tho
 
@@ -91,6 +101,17 @@ int main()
     carigrad = al_load_bitmap("KaoCarigrad.jpg");
     assert(carigrad != NULL);
 
+
+
+    otatt = al_load_bitmap("OtAtt.png");
+    assert(otatt != NULL);
+
+    byatt = al_load_bitmap("ByAtt.png");
+    assert(byatt != NULL);
+
+
+    boom = al_load_bitmap("boom.png");
+    assert(boom != NULL);
 
 
     carigrad1 = al_load_bitmap("CARIGRAD1.png");
@@ -155,6 +176,7 @@ int main()
                 al_clear_to_color(al_map_rgba_f(1, 1, 1, 1));
                 al_draw_bitmap(carigrad,0, 0,0);
 
+                sek = sek + 1;
 
                if (c1 <= 0) {
                     x1 = 1;
@@ -164,22 +186,30 @@ int main()
                 if (c2 <= 0) {
                     x2 = 1;
                     al_draw_bitmap(CRX, 270, 120, 0);
+                    
                 }
 
                 if (c3 <= 0) {
                     x3 = 1;
                     al_draw_bitmap(CRX, 90, 500, 0);
+                    
                 }
 
                 if (c4 <= 0) {
                     x4 = 1;
                     al_draw_bitmap(CRX, 600, 220, 0);
+                    
                }
                 if ((x1+x2+x3+x4) == 4) {
                     al_draw_bitmap(turki,0,0,0);
                 }
 
-                
+                if (o1 <= 0)ox1 = 1;
+                if (o2 <= 0)ox2 = 1;
+                if (o3 <= 0)ox3 = 1;
+                if (o4 <= 0)ox4 = 1;
+                if (o5 <= 0)ox5 = 1;
+                if ((o1+o2+o3+o4+o5)==5)al_destroy_display(display);
 
                 prc += 2;
                 if (prc >= 30)prc = 0;
@@ -194,52 +224,94 @@ int main()
                     prk = attackBroj;
                 }
                 
-                printf("\t%d\t%d %d %d %d\t%d", prk,c1,c2,c3,c4);
+                printf("\t%d\t%d %d %d %d\t%d\n%d", prk,c1,c2,c3,c4,sek);
                 printf("\n");
 
-                al_draw_bitmap(ottoman1, 6, 300, 0);
-                al_draw_bitmap(carigrad1, 120, 300, 0);
+                if (o1>=1)al_draw_bitmap(ottoman1, 6, 300, 0);
+                if (c1 >= 1)al_draw_bitmap(carigrad1, 120, 300, 0);
 
-                al_draw_bitmap(ottoman2, 160, 40, 0);
-                al_draw_bitmap(carigrad2, 270, 120, 0);
+                if (o2 >= 1)al_draw_bitmap(ottoman2, 160, 40, 0);
+                if (c2 >= 1)al_draw_bitmap(carigrad2, 270, 120, 0);
 
-                al_draw_bitmap(ottoman3, -15, 500, 0);
-                al_draw_bitmap(carigrad3, 90, 500, 0);
+                if (o3 >= 1)al_draw_bitmap(ottoman3, -15, 500, 0);
+                if (c3 >= 1)al_draw_bitmap(carigrad3, 90, 500, 0);
 
-                al_draw_bitmap(ottoman4, 90, 160, 0);
+                if (o4 >= 1)al_draw_bitmap(ottoman4, 90, 160, 0);
                 
 
-                al_draw_bitmap(ottoman5, 600, 100, 0);
-                al_draw_bitmap(carigrad4, 600, 220, 0);
+                if (o5 >= 1)al_draw_bitmap(ottoman5, 600, 100, 0);
+                if (c4 >= 1)al_draw_bitmap(carigrad4, 600, 220, 0);
 
+                if (sek > 60)sek = 0;
 
-                if (event.type == ALLEGRO_EVENT_KEY_UP)
+                if (sek > 30)
                 {
+                    al_draw_bitmap(byatt, 750, 600, 0);
+
+                    if(prk%11==0&&o1>=1)
+                    {
+                        o1 = o1 - 2;
+                        al_draw_bitmap(boom, 6, 300, 0);
+
+                    }
+                    if (prk % 21==0&&o2>=1)
+                    {
+                        o2 = o2 - 2;
+                        al_draw_bitmap(boom, 160, 40, 0);
+                    }
+                    if (prk % 27==0&&o3>=1)
+                    {
+                        o3 = o3 - 2;
+                        al_draw_bitmap(boom, -15, 500, 0);
+                    }
+                    if (prk % 19==0&&o4>=1)
+                    {
+                        o4 = o4 - 2;
+                        al_draw_bitmap(boom, 90, 160, 0);
+                    }
+                    if (prk % 32==0&&o5!=1)
+                    {
+                        o5 = o5 - 2;
+                        al_draw_bitmap(boom, 600, 100, 0);
+                    }
+
+
+                }
+
+
+
+                if(sek<=30)al_draw_bitmap(otatt, 750, 600, 0);if (event.type == ALLEGRO_EVENT_KEY_UP)
+                {
+
+                    
+
                     if(prk % 11==0&&c1>=1)
                     {
                         printf("fddfsdfsdfsdfsdfsdfsdfsdfdfs");
                         al_play_sample(Bblizu, 1.0, 0, 1,ALLEGRO_PLAYMODE_ONCE, NULL);
                         c1 = c1 - 2;
-                        
+                        al_draw_bitmap(boom, 120, 300, 0);
                     }
                     if (prk % 21==0&&c2>=1)
                     {
                         printf("fddfsdfsdfsdfsdfsdfsdfsdfdfs");
                         al_play_sample(Bdaleko, 1.0, 0, 1, ALLEGRO_PLAYMODE_ONCE, NULL);
                         c2 = c2 - 2;
+                        al_draw_bitmap(boom, 270, 120, 0);
                     }
                     if (prk % 27 == 0&&c3>=1)
                     {
                         printf("fddfsdfsdfsdfsdfsdfsdfsdfdfs");
                         al_play_sample(Bblizu, 1.0, 0, 1, ALLEGRO_PLAYMODE_ONCE, NULL);
                         c3 = c3 - 2;
+                        al_draw_bitmap(boom, 90, 500, 0);
                     }
                     if (prk % 19 == 0&&c4>=1)
                     {
                         printf("fddfsdfsdfsdfsdfsdfsdfsdfdfs");
                         al_play_sample(Bdaleko, 1.0, 0, 1, ALLEGRO_PLAYMODE_ONCE, NULL);
                         c4 = c4 - 2;
-
+                        al_draw_bitmap(boom, 600, 220, 0);
                     }
                 }
                 
@@ -263,4 +335,4 @@ int main()
 
     return 0;
 }
-//Rad Mateje Mihajlovicha 1-4 Gimnazija Chuprija 2022 6/18 :))))))))))))))
+//Rad Mateje Mihajlovicha 1-4 Gimnazija Chuprija 2022 6/18,7/18 i neki dani pre toga najjache :))))))))))))))
